cc84b9e9ec1a966af8a7b5aaeedd79a7dc6056d5
Brian Fernando <brianfdo@users.noreply.github.com>
2021-07-21 19:45:31 -0400

    updated algo

--- wood_cutting.py
+++ wood_cutting.py
@@ -9,19 +9,46 @@ cut_list.sort(reverse=True)
 # hard coded sheet size
 sheet_size = 250
 
+# max number of sheets
+max_sheets = len(cut_list)
 
-def cut(cut_list, sheet_size):
+def cut(cut_list, sheet_size, max_sheets):
     sheets = 0
-    rem_length = sheet_size
+    rem_sheets = [0] * max_sheets
+    # rem_length = sheet_size
     cuts_per_sheet = []
     temp = []
-    for i in range(len(cut_list)):
-        if rem_length >= cut_list[i]:
-            rem_length = rem_length - cut_list[i]
-            temp.append(cut_list[i])
-        else:
+
+    # loops through all cuts
+    for i in range(max_sheets):
+        j = 0
+
+        min = sheet_size + 1
+        best_index = 0
+        # loop to find best sheet to assign the cut
+        for j in range(sheets):
+            if (rem_sheets[j] >= cut_list[i] and rem_sheets[j] - cut_list[i] < min):
+                best_index = j
+                min = rem_sheets[j] - cut_list[i]
+             
+        # if no sheet could accommodate the cut, then we create a new bin
+        if (min == sheet_size + 1):
+            rem_sheets[sheets] = sheet_size - cut_list[i]
+            cuts_per_sheet.append([cut_list[i]])
             sheets += 1
-            cuts_per_sheet.append(temp)
-            rem_length = sheet_size - cut_list[i]
-            temp = [cut_list[i]]
-    return(sheets, cuts_per_sheet)
+        # assign the cut to best sheet
+        else: 
+            rem_sheets[best_index] -= cut_list[i]
+            cuts_per_sheet[best_index].append(cut_list[i])
+
+        # if rem_length >= cut_list[i]:
+        #     rem_length = rem_length - cut_list[i]
+        #     temp.append(cut_list[i])
+        # else:
+        #     sheets += 1
+        #     cuts_per_sheet.append(temp)
+        #     rem_length = sheet_size - cut_list[i]
+        #     temp = [cut_list[i]]
+    return(sheets, cuts_per_sheet, 'waste per sheet:', rem_sheets[:sheets])
+
+print(cut(cut_list, 250, len(cut_list)))
